cc6edc6714b0ef56e97141aaf252e4ecde25ce9d
Amandeep Yadav <aman08jul@gmail.com>
2025-03-23 15:22:00 +0530

    add tailwind

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class PageController {
-    
-    @RequestMapping("/home")//whichever attribute will be added to it it will automatically be added to template
-    public String home(Model model) {
-        System.out.println("Home page Handler");//error either template is not there or not acessible 
-        model.addAttribute("name","substring technology");
-        model.addAttribute("YoutubeChannel", "make the world ");
-        model.addAttribute("GithubRepository", "ricky08sirus");
-        return "home";
-    }
 
+  @RequestMapping("/home") // whichever attribute will be added to it it will automatically be added to
+                           // template
+  public String home(Model model) {
+    System.out.println("Home page Handler");// error either template is not there or not acessible
+    // sending data to view
+    model.addAttribute("name", "substring technology");
+    model.addAttribute("YoutubeChannel", "make the world ");
+    model.addAttribute("GithubRepository", "https://github.com/ricky08sirus/SCM2.0");
+    return "home";
+  }
 
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./src/main/resources/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
